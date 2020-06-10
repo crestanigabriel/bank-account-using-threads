454acf8a00346702d4720bf8b10337531b3eaf2b
Rafaella Bambokian <rafaellabambokian@outlook.com>
2020-06-10 15:14:38 -0400

    Call functions - main

--- main.cpp
+++ main.cpp
@@ -11,11 +11,21 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Hello world!\n\n" << endl;
     Bank bank;
-    bank.insert_account(234.0, "Ana");
+    bank.insert_account(200.0, "Ana");
+    bank.insert_account(43345.34, "B");
+    bank.insert_account(5465.3, "C");
     bank.print_accounts();
-
-
+    
+    bank.deposit(10, "Ana");
+    bank.drawOut(250, "Ana");
+    bank.drawOut(205, "Ana");
+    
+    bank.account_updater(0.12);
+    
+    bank.print_accounts();
+    
+    
     return 0;
 }
