79c0c80cb58831d8bc4665d527699862520ee3bf
Rafaella Bambokian <rafaellabambokian@outlook.com>
2020-06-10 15:16:48 -0400

    Add drawOut method

--- include/Account.h
+++ include/Account.h
@@ -7,20 +7,21 @@ using namespace std;
 
 class Account
 {
-    public:
-        Account(double balance, string owner);
-        virtual ~Account();
-
-        void update(double tax);
-        void deposit(double value);
-        double get_balance();
-        string get_owner();
-
-    protected:
-
-    private:
-        double _balance;
-        string _owner;
+public:
+    Account(double balance, string owner);
+    virtual ~Account();
+    
+    void update(double tax);
+    void deposit(double value);
+    void drawOut(double value);
+    double get_balance();
+    string get_owner();
+    
+protected:
+    
+private:
+    double _balance;
+    string _owner;
 };
 
 #endif // ACCOUNT_H
