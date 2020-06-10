e5f5e3378695bc340d6156a2d5cba3444777166f
Rafaella Bambokian <rafaellabambokian@outlook.com>
2020-06-10 15:18:22 -0400

    Update account_updater method

--- include/Bank.h
+++ include/Bank.h
@@ -11,19 +11,20 @@ using namespace std;
 
 class Bank
 {
-    public:
-        Bank();
-        virtual ~Bank();
-
-        void insert_account(double balance, string owner);
-        void deposit(double value, string owner);
-        void account_updater();
-        void print_accounts();
-
-    protected:
-
-    private:
-        list<Account> accounts;
+public:
+    Bank();
+    virtual ~Bank();
+    
+    void insert_account(double balance, string owner);
+    void deposit(double value, string owner);
+    void drawOut(double value, string owner);
+    void account_updater(double tax);
+    void print_accounts();
+    
+protected:
+    
+private:
+    list<Account> accounts;
 };
 
 #endif // BANK_H
