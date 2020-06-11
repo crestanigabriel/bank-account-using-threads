c26e4d93d07b172ffac2de425df65e4220d89f0d
crestanigabriel <gabrielpcrestani@gmail.com>
2020-06-11 13:40:44 -0300

    Multiple_operations function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include <list>
 
 #include <Bank.h>
@@ -6,6 +7,7 @@
 #include <Semaphore.h>
 
 #define NUMBER_OF_ACCOUNTS 10
+#define NUMBER_OF_OPERATIONS 100
 
 using namespace std;
 
@@ -14,14 +16,32 @@ void create_accounts(Bank &bank) {
         bank.insert_account(10.0 * i * i, "Client" + to_string(i+1));
 }
 
+void multiple_operations(Bank &bank) {
+    for (int i = 0; i < NUMBER_OF_OPERATIONS; i++) {
+        int operation = rand()%2; // 0 -> drawout, 1 -> deposit
+        double value = ((double)rand() / RAND_MAX) * 1000.0; // double number between 0 and 1000
+        string client = "Client" + to_string(rand()%NUMBER_OF_ACCOUNTS + 1);
+
+        switch (operation) {
+            case 0: bank.drawOut(value, client); break;
+            case 1: bank.deposit(value, client); break;
+        }
+        // Performs account_updater 10 times
+        if ((i+1) % (NUMBER_OF_OPERATIONS/10) == 0) {
+            double tax = (double)rand() / RAND_MAX; // double number between 0 and 1
+            bank.account_updater(tax);
+        }
+    }
+}
+
 int main()
 {
     Bank bank;
-
     create_accounts(bank);
+
     bank.print_accounts();
 
-    bank.account_updater(0.12);
+    multiple_operations(bank);
 
     bank.print_accounts();
 
